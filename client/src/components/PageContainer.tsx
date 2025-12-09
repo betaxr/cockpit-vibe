e08510878103c6b99c21941d2905740a20242987
elermi <58939937+noColourinks@users.noreply.github.com>
2025-12-09 18:06:24 +0100

    Add margin-top to PageContainer for improved layout spacing

--- client/src/components/PageContainer.tsx
+++ client/src/components/PageContainer.tsx
@@ -7,7 +7,7 @@ type PageContainerProps = {
 
 export default function PageContainer({ children, className }: PageContainerProps) {
   return (
-    <div className={cn("max-w-5xl mx-auto px-6 pt-10 pb-12", className)}>
+    <div className={cn("max-w-5xl mx-auto px-6 pt-10 pb-12 mt-[15vh]", className)}>
       {children}
     </div>
   );
